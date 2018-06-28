907f259fcf1a6a329faba346979b299c6aa8570d
hitheima <hitheima@163.com>
2018-06-28 18:34:11 +0800

    commit

--- test_search/base/__init__.py
+++
@@ -1,3 +0,0 @@
-from .base_driver import init_driver
-from .base_analyze import analyze_file
-from .base_action import BaseAction
--- test_search/base/base_analyze.py
+++
@@ -1,6 +0,0 @@
-import yaml
-
-
-def analyze_file():
-    with open("./data/search_data.yml", 'r') as f:
-        return yaml.load(f)
